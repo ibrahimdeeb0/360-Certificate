0ad1e396c7cd7b303c6557efa04dd1dff2597498
Mohammed Alshayah <90424258+Mohammed-Al-Shayah@users.noreply.github.com>
2023-07-30 12:01:16 +0300

    fix the image and note in Minor Works

--- lib/screens/forms/electrical_forms/minor_works/minor_form_attachments/minor_add_images.dart
+++ lib/screens/forms/electrical_forms/minor_works/minor_form_attachments/minor_add_images.dart
@@ -5,176 +5,160 @@ class MinorAddImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: Header(
-        title: 'Images & Notes',
-        withShadow: true,
-      ),
-      body: GetBuilder<MinorAttachmentsController>(
-        init: MinorAttachmentsController(),
-        builder: (MinorAttachmentsController controller) {
-          return CommonContainer(
-            style: appContainerStyles.containerStyles,
-            paddingTop: 0.02,
-            child: controller.imagesData.isNotEmpty
-                ? Stack(
-                    children: <Widget>[
-                      SingleChildScrollView(
-                        child: Column(
-                          children: <Widget>[
-                            CommonText(
-                              'Uploaded Images',
-                              fontColor: AppColors.primary,
-                              fontSize: fontH2,
-                              marginBottom: 0.02,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            if (controller.imagesData.isNotEmpty)
-                              ...controller.imagesData.map(
-                                (FormImageClass item) {
-                                  final int itemIndex =
-                                      controller.imagesData.indexOf(item);
+    return GetBuilder<MinorAttachmentsController>(
+      init: MinorAttachmentsController(),
+      builder: (MinorAttachmentsController controller) {
+        return Scaffold(
+            appBar: Header(
+              title: 'Images & Notes',
+              withShadow: true,
+              actionItem: ActionItem(type: ActionType.save),
+              onPressSave: () {},
+            ),
+            floatingActionButton: Visibility(
+              visible: controller.imagesData.isNotEmpty,
+              child: FloatingActionButton(
+                onPressed: () {
+                  controller.pickImage();
+                },
+                backgroundColor: Color(AppColors.primary),
+                child: const Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            body: CommonContainer(
+                style: appContainerStyles.containerStyles,
+                paddingTop: 0.02,
+                child: controller.imagesData.isNotEmpty
+                    ? Column(
+                        children: <Widget>[
+                          SingleChildScrollView(
+                            child: Column(
+                              children: <Widget>[
+                                CommonText(
+                                  'Uploaded Images',
+                                  fontColor: AppColors.primary,
+                                  fontSize: fontH2,
+                                  marginBottom: 0.02,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                if (controller.imagesData.isNotEmpty)
+                                  ...controller.imagesData.map(
+                                    (FormImageClass item) {
+                                      final int itemIndex =
+                                          controller.imagesData.indexOf(item);
 
-                                  return FormAddImageCard(
-                                    fileName: item.file.path
-                                        .split('/')
-                                        .last
-                                        .split('picker')
-                                        .last,
-                                    imagePath: item.file.path,
-                                    pressToggleInclude: () {
-                                      controller.imagesData[itemIndex]
-                                          .isIncluded = !item.isIncluded;
-                                      controller.update();
-                                    },
-                                    isIncluded: item.isIncluded,
-                                    pressView: () {
-                                      Get.dialog(
-                                        ViewImageContainer(
-                                          imagePath: item.file.path,
-                                          imageType: ImageFormatType.local,
-                                          isFullScreen: true,
-                                        ),
-                                      );
-                                    },
-                                    pressDelete: () {
-                                      openDialog(
-                                        child: CommonText(
-                                          'Are You Sure want to Delete?',
-                                          marginTop: 0.02,
-                                          topChild: Icon(
-                                            Icons.delete,
-                                            size: 0.07.flexAll,
-                                            color: Colors.red[900],
-                                          ),
-                                        ),
-                                        onCancel: () {
-                                          if (Get.isDialogOpen!) {
-                                            Get.back();
+                                      return FormAddImageCard(
+                                        fileName: item.file.path
+                                            .split('/')
+                                            .last
+                                            .split('picker')
+                                            .last,
+                                        imagePath: item.file.path,
+                                        pressToggleInclude: () {
+                                          controller.imagesData[itemIndex]
+                                              .isIncluded = !item.isIncluded;
+                                          controller.update();
+                                        },
+                                        isIncluded: item.isIncluded,
+                                        pressView: () {
+                                          Get.dialog(
+                                            ViewImageContainer(
+                                              imagePath: item.file.path,
+                                              imageType: ImageFormatType.local,
+                                              isFullScreen: true,
+                                            ),
+                                          );
+                                        },
+                                        pressDelete: () {
+                                          openDialog(
+                                            child: CommonText(
+                                              'Are You Sure want to Delete?',
+                                              marginTop: 0.02,
+                                              topChild: Icon(
+                                                Icons.delete,
+                                                size: 0.07.flexAll,
+                                                color: Colors.red[900],
+                                              ),
+                                            ),
+                                            onCancel: () {
+                                              if (Get.isDialogOpen!) {
+                                                Get.back();
+                                              }
+                                            },
+                                            onConfirm: () {
+                                              if (Get.isDialogOpen!) {
+                                                Get.back();
+                                              }
+                                              item.onPress?.call();
+                                            },
+                                            title: '',
+                                          );
+                                        },
+                                        pressNote: () {
+                                          if (item.note == null) {
+                                            Get.to(
+                                              () => const MinorWriteNote(
+                                                fromImage: true,
+                                              ),
+                                              arguments: <String, dynamic>{
+                                                'index': itemIndex,
+                                              },
+                                            );
+                                          } else {
+                                            controller.noteController.text =
+                                                item.note!;
+                                            Get.to(
+                                              () => const MinorWriteNote(
+                                                fromImage: true,
+                                              ),
+                                              arguments: <String, dynamic>{
+                                                'index': itemIndex,
+                                              },
+                                            );
                                           }
                                         },
-                                        onConfirm: () {
-                                          if (Get.isDialogOpen!) {
-                                            Get.back();
-                                          }
-                                          item.onPress?.call();
-                                        },
-                                        title: '',
+                                        note: item.note,
                                       );
                                     },
-                                    pressNote: () {
-                                      if (item.note == null) {
-                                        Get.to(
-                                          () => const MinorWriteNote(
-                                            fromImage: true,
-                                          ),
-                                          arguments: <String, dynamic>{
-                                            'index': itemIndex,
-                                          },
-                                        );
-                                      } else {
-                                        controller.noteController.text =
-                                            item.note!;
-                                        Get.to(
-                                          () => const MinorWriteNote(
-                                            fromImage: true,
-                                          ),
-                                          arguments: <String, dynamic>{
-                                            'index': itemIndex,
-                                          },
-                                        );
-                                      }
-                                    },
-                                    note: item.note,
-                                  );
-                                },
-                              ),
-                          ],
-                        ),
-                      ),
-                      Positioned(
-                        bottom: 0,
-                        left: 0,
-                        right: 0,
-                        child: Padding(
-                          padding: EdgeInsets.only(bottom: 0.02.flexHeight),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              CommonButton(
-                                onPress: Get.back,
-                                text: 'Cancel',
-                                width: 0.45,
-                                backgroundColor: Colors.white,
-                                fontColor: AppColors.primary,
-                                borderWidth: 1,
-                                borderColor: AppColors.primary,
-                              ),
-                              CommonButton(
-                                onPress: () {
-                                  controller.pickImage();
-                                },
-                                text: 'Upload',
-                                width: 0.45,
-                              ),
-                            ],
+                                  ),
+                              ],
+                            ),
                           ),
-                        ),
-                      ),
-                    ],
-                  )
-                : Column(
-                    children: <Widget>[
-                      CommonText(
-                        'Uploaded Images',
-                        fontColor: AppColors.primary,
-                        fontSize: fontH2,
-                        marginBottom: 0.02,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      CommonText(
-                        'No data available',
-                        fontColor: Colors.grey[700],
-                        marginBottom: 0.1,
-                      ),
-                      CommonButton(
-                        onPress: () {
-                          controller.pickImage();
-                        },
-                        child: const CommonText(
-                          'Upload Image',
-                          fontColor: Colors.white,
-                          leftChild: Icon(
-                            Icons.file_upload_outlined,
-                            color: Colors.white,
+                        ],
+                      )
+                    : Column(
+                        children: <Widget>[
+                          CommonText(
+                            'Uploaded Images',
+                            fontColor: AppColors.primary,
+                            fontSize: fontH2,
+                            marginBottom: 0.02,
+                            fontWeight: FontWeight.bold,
                           ),
-                        ),
-                      ),
-                    ],
-                  ),
-          );
-        },
-      ),
+                          CommonText(
+                            'No data available',
+                            fontColor: Colors.grey[700],
+                            marginBottom: 0.1,
+                          ),
+                          CommonButton(
+                            onPress: () {
+                              controller.pickImage();
+                            },
+                            child: const CommonText(
+                              'Upload Image',
+                              fontColor: Colors.white,
+                              leftChild: Icon(
+                                Icons.file_upload_outlined,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )));
+      },
     );
   }
 }
