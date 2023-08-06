0014da692f8ff516cf5f7ab0b22cb6f8d519bb1d
Mohammed Alshayah <90424258+Mohammed-Al-Shayah@users.noreply.github.com>
2023-08-06 12:42:23 +0300

    Sort the customers list in an alphabetical order

--- lib/screens/search_for_customer/search_for_customer.dart
+++ lib/screens/search_for_customer/search_for_customer.dart
@@ -14,6 +14,9 @@ class SearchForCustomer extends StatelessWidget {
       body: GetBuilder<SearchForCustomerController>(
         init: SearchForCustomerController(),
         builder: (SearchForCustomerController controller) {
+          controller.searchResult
+              .sort((a, b) => a['f_name'].compareTo(b['f_name']));
+
           return CommonContainer(
             style: appContainerStyles.containerStyles,
             paddingHorizontal: 0,
