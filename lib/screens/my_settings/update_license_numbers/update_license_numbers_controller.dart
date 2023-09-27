1783f46a06035657bcfe32200df07279abb157e8
Mohammed-Al-Shayah <mohammedalshayah@gmail.com>
2023-09-27 10:06:56 +0300

    license number

--- lib/screens/my_settings/update_license_numbers/update_license_numbers_controller.dart
+++ lib/screens/my_settings/update_license_numbers/update_license_numbers_controller.dart
@@ -3,9 +3,71 @@ import '../../../general_exports.dart';
 class UpdateLicenseNumbersController extends GetxController {
   TextEditingController gasNumController = TextEditingController();
   TextEditingController electricalNumController = TextEditingController();
+  bool isboardFrom = false;
 
   final CompleteProfileController completeProfilecontroller =
-              CompleteProfileController();
+      CompleteProfileController();
+
+  List<Map<String, dynamic>> selectedElectricBoard = <Map<String, dynamic>>[];
+  List<int> electricBoardId = <int>[];
+
+  List<Map<String, dynamic>> listElectricBoard = <Map<String, dynamic>>[
+    <String, dynamic>{
+      keyId: 1,
+      keyName: 'ELECSA',
+    },
+    <String, dynamic>{
+      keyId: 2,
+      keyName: 'STROMA',
+    },
+    <String, dynamic>{
+      keyId: 3,
+      keyName: 'IET (Institution of Engineering and Technology)',
+    },
+    <String, dynamic>{
+      keyId: 4,
+      keyName: 'JIB (Joint Industry Board)',
+    },
+    <String, dynamic>{
+      keyId: 5,
+      keyName: 'SELECT (Scotland)',
+    },
+    <String, dynamic>{
+      keyId: 6,
+      keyName: 'NAPIT',
+    },
+    <String, dynamic>{
+      keyId: 7,
+      keyName: 'NICEIC',
+    },
+  ];
+
+  // electricBoardId
+  void onSelectElectricBoard(Map<String, dynamic> item) {
+    final String itemId = item[keyId].toString();
+
+    if (selectedElectricBoard.isEmpty ||
+        selectedElectricBoard[0][keyId] != itemId) {
+      selectedElectricBoard.clear();
+      selectedElectricBoard.add(item);
+
+      electricBoardId = <int>[int.parse(itemId)];
+    } else {
+      selectedElectricBoard.clear();
+      electricBoardId.clear();
+    }
+
+    consoleLog(electricBoardId, key: 'electricBoardId');
+
+    update();
+  }
+
+  @override
+  void onReady() {
+    getBoardFrom();
+    update();
+    super.onReady();
+  }
 
   @override
   void onInit() {
@@ -13,6 +75,53 @@ class UpdateLicenseNumbersController extends GetxController {
     electricalNumController.text = homeController.electricNumber ?? '';
     update();
     super.onInit();
+  }
+
+  int? boardFrom;
+
+  Map<String, dynamic>? selectedBoard;
+  void getBoardFrom() {
+    hideKeyboard();
+    startLoading();
+
+    ApiRequest(
+      className: 'UpdateCertNumberController/boardFrom',
+      path: '/getLicenseDetails',
+      requestFunction: getBoardFrom,
+      withLoading: true,
+      formatResponse: true,
+    ).request(
+      onSuccess: (dynamic data, dynamic response) {
+        // final dynamic jsonData = jsonDecode(data);
+        // boardFrom = jsonData[0][0];
+        boardFrom = data;
+        selectedBoard = listElectricBoard
+            .where((Map<String, dynamic> element) => element['id'] == boardFrom)
+            .first;
+
+        consoleLog(boardFrom, key: 'board_value_id');
+        update();
+      },
+    );
+  }
+
+  void onUpdateboardFrom() {
+    hideKeyboard();
+    ApiRequest(
+      className: 'UpdateCertNumberController/onUpdateboardFrom',
+      method: ApiMethods.post,
+      path: '/update-electricboard/${electricBoardId[0]}',
+      requestFunction: onUpdateboardFrom,
+      withLoading: true,
+      formatResponse: true,
+    ).request(
+      onSuccess: (dynamic data, dynamic response) {
+        Get.back();
+      },
+      onError: (dynamic error) {
+        dismissLoading();
+      },
+    );
   }
 
   void onUpdateCertNumber() {
